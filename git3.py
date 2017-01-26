364003a5fe0d0241ba9f456c9b88bd974a450491
thordurjo364 <thordurjo364@nemi.tskoli.is>
2017-01-26 10:51:26 +0000

    dæmi 3

--- git3.py
+++ git3.py
@@ -14,3 +14,19 @@ print("tölurnar margfaldaðar : ",tala1*tala2)
 fornafn=input("Sláðu inn fornafn : ")
 eftirnafn=input("Sláðu inn eftirnafn : ")
 print("Halló ",fornafn, eftirnafn)
+
+#Dæmi 3
+hastafir=0
+lastafir=0
+laghastaf=0
+
+texti=input("Sláðu inn texta : ")
+for i in range(len(texti)):
+    if texti[i].isupper():
+        hastafir=hastafir+1
+
+    if texti[i].islower():
+        lastafir=lastafir+1
+
+print(texti)
+print("það eru",hastafir,"Hástafir og",lastafir,"lástafir")
