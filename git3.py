b1d2ee5567b90ef09e8485881121efbac437e7c3
thordurjo364 <thordurjo364@nemi.tskoli.is>
2017-01-26 10:50:39 +0000

    Dæmi 2

---
+++ git3.py
@@ -0,0 +1,16 @@
+#24.01.2017
+#Þórður Jónatansson
+#Git/pycharm verkefni
+
+
+#Dæmi 1
+tala1=int(input("Sláðu inn tölu"))
+tala2=int(input("Sláðu inn aðra tölu"))
+print("tölurnar lagðar saman : ",tala1+tala2)
+print("tölurnar margfaldaðar : ",tala1*tala2)
+
+
+#Dæmi 2
+fornafn=input("Sláðu inn fornafn : ")
+eftirnafn=input("Sláðu inn eftirnafn : ")
+print("Halló ",fornafn, eftirnafn)
